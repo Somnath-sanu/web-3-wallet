e467994b51f2c95f805868d5e32425995bd45d0a
somnath <somnathmishra100dbi@gmail.com>
2024-09-06 00:50:29 +0530

    fix2

--- components/Verification.tsx
+++ components/Verification.tsx
@@ -13,7 +13,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 
-import Gif from "@/assets/fox-gif.gif";
+import Gif from "../assets/fox-gif.gif";
 import { useEffect, useState } from "react";
 import { useToast } from "@/hooks/use-toast";
 import {
